24d49000c327d5d96a4c5bebb2dec1b9dcc16697
Dylan Colli <dylanfrankcolli@gmail.com>
2023-12-13 15:31:25 -0500

    Make repository initialization one step

--- init_repo.sh
+++ init_repo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Cloning benchmark repositories"
+echo "------------------------------"
 # Initializes the repository by pulling some of the GPU profiling repositories. Opted to use this
 # script instead of git submodules since those can be a bit of a headache.
 git clone https://github.com/UM-ARM-Lab/arm_segmentation.git
@@ -9,6 +11,37 @@ git clone https://github.com/UM-ARM-Lab/pytorch_kinematics.git
 # Currently having trouble with the MPPI autotune pip installation.
 # git clone https://github.com/UM-ARM-Lab/pytorch_mppi.git
 
-echo "Repository initialized."
 echo ""
-echo "Please install the cloned repositories into your Python environment."
+
+echo "Creating Python virtual environment (pipenv) file"
+echo "-------------------------------------------------"
+# This Python file only uses standard libraries so it doesn't need to be ran in any sort of
+# environment. As long as the user has a Python 3 interpreter installed and on their path, they'll
+# be fine.
+python3 populate_pipenv_template.py
+echo ""
+
+# Check if pipenv is installed.
+if ! command -v pipenv &> /dev/null
+then
+    echo "pipenv could not be found. Please install with:"
+    echo '    `python3 -m pip install pipenv`'
+    echo "and run:"
+    echo '    `pipenv install`'
+    echo "in this repositories directory."
+    exit 1
+fi
+
+# TODO: If not installed, error out and ask user to install pipenv themself.
+
+echo "Creating pipenv environment if necessary"
+echo "----------------------------------------"
+pipenv install
+echo ""
+
+
+echo "Repository initialized!"
+echo "You can now run the benchmarks. Simply activate the pipenv environment with:"
+echo '    `pipenv shell`'
+echo "and run:"
+echo '    `python run_benchmarks.py`'
